1311e6e8f8e15f19115348829990047122497a28
avash007thapa <58122425+avash007thapa@users.noreply.github.com>
2020-11-08 18:42:01 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
-#include "stack.hpp"
+// driver code belongs here
 
 #include <iostream>
+#include "stack.hpp"
 //Gurnek was here
 // Avash was here
 
 
 //Testing Merge file
+using namespace std;
+using namespace ENSC251_lab4;
 
-int main {
+int main(){
+    
+    Stack<int> stm1h;
 
     return 0;
 }
